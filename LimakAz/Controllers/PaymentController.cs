fbe10f6a5ce551e04409387a97b8f83f3c90ce87
Simara08 <7knywpc@code.edu.az>
2022-12-03 11:45:59 +0400

    kargo project completed

---
+++ LimakAz/Controllers/PaymentController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using LimakAz.Models.Payment;
+using LimakAz.Payment;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LimakAz.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Page1()
+        {
+            return View();
+        }
+        public IActionResult Page2()
+        {
+            return View();
+        }
+        public IActionResult Page3()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+
+        [Route("pay")]
+        public async Task<dynamic> Pay(PayModel payModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await ProcessPayment.PayAsync(payModel);
+
+                if (result == "Success")
+                {
+                    return RedirectToAction("Success");
+                }
+                else
+                {
+                    return RedirectToAction("Error", result);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error(string error)
+        {
+            return View(new ErrorViewModel { RequestId = error != null ? error : Activity.Current?.Id ?? HttpContext.TraceIdentifier }); ;
+        }
+    }
+}
---
+++ LimakAz/ViewModels/BalanceViewModel.cs
@@ -0,0 +1,16 @@
+﻿using LimakAz.Models;
+using LimakAz.Models.Payment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LimakAz.ViewModels
+{
+    public class BalanceViewModel
+    {
+        public double Money { get; set; }
+        public AppUser Member { get; set; }
+        public PayModel payModel { get; set; }
+    }
+}
